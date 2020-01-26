67004bff965414376c213034a9336a2d36812774
Brianna Pereira <bpereira2@usf.edu>
2020-01-25 21:57:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
